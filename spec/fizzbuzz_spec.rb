154c763967db944cbf88f741cf5953f890c1a4f0
Charlene Bastians <charlenebastians@hotmail.com>
2018-02-12 20:09:01 +0000

    rspec_and_spec_added

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,43 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'fizz' when passed 9" do
+    expect(fizzbuzz(9)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'buzz' when passed 20" do
+    expect(fizzbuzz(20)).to eq "buzz"
+  end
+end
+  
+describe "fizzbuzz" do
+  it "returns 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+end
+ 
+describe "fizzbuzz" do
+  it "returns 'fizzbuzz' when passed 30" do
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 14 when passed 14" do
+    expect(fizzbuzz(14)).to eq 14
+  end
+end
